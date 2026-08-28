31bf14c56d5a721dd77ff28c667d2afdb6af609e
NOOMTUTEE <jinjurat13891389@gmail.com>
2026-08-28 15:57:52 +0700

    feat: add reserveroute

--- src/routes/reserve.route.js
+++ src/routes/reserve.route.js
@@ -6,3 +6,11 @@ const reserveRoute = express.Router()
 // Cars
 reserveRoute.use('/cars', (req, res)=>{res.send("getcars")} )
 reserveRoute.post('/', (req, res)=>{res.send("postcars")})
+reserveRoute.patch('/carId',(req, res)=>{res.send("carId")})
+
+// Rooms
+reserveRoute.use('/rooms', (req, res)=>{res.send("getrooms")})
+reserveRoute.post('/rooms', (req, res)=>{res.send("postrooms")})
+reserveRoute.patch('/roomId', (req, res)=>{res.send("roomId")})
+
+export default reserveRoute
